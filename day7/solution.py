acebfc9a001293c096b5a7b185eb01dd96fc28fd
Jan Toth <jan.toth@external.erstegroup.com>
2022-12-08 15:09:02 +0100

    Adding working day 7

--- day7/solution.py
+++ day7/solution.py
@@ -14,7 +14,7 @@ with open(args.file) as f:
 
 level = [-1]
 t = list()
-chdir = ["/"]
+chdir = []
 dec = 0
 
 couples = []
@@ -26,67 +26,99 @@ for i in lines:
             if re.search('^[1-9]', _e):
                 tpp.append(_e.split(' ')[0])
             else:
-                tpp.append(f"{level[-1]+1}{_e.split(' ')[1]}")
+                tpp.append(f"{_e.split(' ')[1]}")
 
-        couples.append([level[-1], chdir[-1], tpp])
-        # below the magic had to be done when it comes to decrementing
+
+        chdir.append(i.split(" ")[-1])
+        #print(level[-1], '/'.join(chdir[:-1]), tpp)
+        couples.append([level[-1], '/'.join(chdir[:-1]), tpp])
+
+        #  fix chdir list if there was decrement before
+        if dec != 0:
+            chdir = chdir[:dec - 1]
+            chdir.append(i.split(" ")[-1])
+
         if dec != 0:
             _t = level[-1] + dec
             level.pop()
             level.append(_t)
             dec = 0
+
         level.append(level[-1] + 1)
-        chdir.append(i.split(" ")[-1])
         t = list() # clear up list
         tpp = list()
+        #print()
     elif re.search('^.*\.\.$', i):
         dec -= 1
     elif re.search('^d|[1-9]', i):
+        #print("adding:", i)
         t.append(i)
-    time.sleep(0.0002)
-
-#print(level[-1], chdir[-1], t)
+    time.sleep(0.0000005)
 
 tpp = list()
 for _e in t:
     if re.search('^[1-9]', _e):
         tpp.append(_e.split(' ')[0])
     else:
-        tpp.append(f"{level[-1]+1}{_e.split(' ')[1]}")
-couples.append([level[-1], chdir[-1], tpp])
+        tpp.append(f"{_e.split(' ')[1]}")
+
+#print(level[-1], '/'.join(chdir), tpp)
+couples.append([level[-1], '/'.join(chdir), tpp])
 tpp = list()
 
-
-# ....................
-
+# END OF LOOPING
 e = sorted(couples, key=lambda x: x[0])
-o = e[::-1][:-2]
-
-# create dictionary of unique keys
-spec = {f"{i[0]}{i[1]}": i[2] for i in o}
-
-print(o)
-print(spec)
-
-def convertme(_list, _dict):
-   tmp = []
-   for i in _list:
-       try:
-           tmp.append(_dict[i][0])
-       except:
-           tmp.append(i)
-   return tmp
-
-part1 = 0
+#o = e[::-1][:-2]
+o = e[::-1][:-1]
 
 for i in o:
-    #print(spec[f"{i[0]}{i[1]}"])
-    x = convertme(spec[f"{i[0]}{i[1]}"] ,spec)
-    s = sum([int(i) for i in x])
-    # print(f"{i[:2]}", s)
-    if s < 100000:
-        print(f"{i[:2]}", s)
-        part1 +=s
+    #print("before remake:", i)
+    # add full paths to lists of file sizes 
+    # if folder inside this list
+    _t = []
+    for x in i[2]:
+        if re.search('^[a-z]', x):
+            _t.append(f"{i[1]}/{x}")
+        else:
+            _t.append(x)
+    i[2] = _t
 
-print(f"Part 1: {part1}")
+# create dictionary of unique keys
+spec = {f"{i[1]}": i[2] for i in o}
+part1 = 0
+
+# Must update dict named "spec" in place dude !!!
+
+for item in o:
+    # item[1] <--- key
+    # item[2] <--- value (defacto list that needs to be transformed)
+    #print(item[1:])
+    tempo = []
+    for element in item[2]:
+        # if element is path istead of number
+        if not re.search('^[0-9]', element):
+            tempo.append(spec[element])
+        else:
+            tempo.append(element)
+    spec[item[1]] = tempo
+
+
+p1 = 0
+p2 = []
+total = sum([int(i) for i in re.findall('[0-9]+', str(spec['/']))])
+needed = 30000000 - abs(total - 70000000)
+print("total:", total, "needed:", needed)
+
+for k,v in spec.items():
+    nums = sum([int(i) for i in re.findall('[0-9]+', str(v))])
+    print(nums)
+    if nums < 100000:
+        p1 += nums
+    if nums > needed:
+        p2.append(nums)
+print("Part1:", p1)
+print("Part2:", min(p2))
+
+
+
 
