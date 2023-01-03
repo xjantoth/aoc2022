abc06b9a6a8791fceabb92a662ce7213735cf714
Jan Toth <jan.toth@external.erstegroup.com>
2023-01-03 12:09:28 +0100

    Adding working day 11 second part thanks to Krissko and Vilko

--- day11/solution.py
+++ day11/solution.py
@@ -26,24 +26,34 @@ for i in data:
 
 m = processme(tmp, m, c)
 
+def lcc(_m):
+    lc = []
+    for i in _m:
+        for l in _m[i][0]:
+            lc.append(l)
+
+    return math.lcm(*lc)
+
 count = 20
 #count = 10000
 
+nlcm = lcc(m)
+
 while 0 < count:
-    print(f"*********************** {count} *************************")
     for i in m:
-        #print(i, m[i])
         for e, n in enumerate(m[i][0]):
+
             m[i][5] += 1 # how many times monkey inspected an item
-            expr = re.sub('old', str(n), m[i][1])
-            worry_lvl = eval(expr) // 3
-            #worry_lvl = eval(expr) // math.gcd(*m[i][0])
+            expr = re.sub('old', str(n), m[i][1]) # old * 19
+            #worry_lvl = eval(expr) // 3  # Part 1
+            worry_lvl = eval(expr) % nlcm # Part 2
             condition = (worry_lvl) % int(re.findall('[0-9]+', m[i][2])[0]) == 0
             monkey = re.findall('[0-9]+',m[i][3])[0] if condition else re.findall('[0-9]+',m[i][4])[0]
-            #print(f"monkey {i}, item: {n} - {eval(expr)}, {eval(expr) // 3}  --> goes to m {monkey}, wl {worry_lvl}")
-            #print(f"monkey {i}, item: {n} - {eval(expr)}, {eval(expr)}  --> goes to m {monkey}, wl {worry_lvl}")
+            worry_lvl = eval(expr) % nlcm
+            condition = (worry_lvl) % int(re.findall('[0-9]+', m[i][2])[0]) == 0
+            print(f"monkey {i}, item: {n} -> goes to {monkey}, wl {worry_lvl}")
             m[int(monkey)][0].append(worry_lvl) # pushing newly calculated worry level to a respective monkey
-            #time.sleep(1)
+            #time.sleep(0.1)
         m[i][0] = [] # deleting currently processed worry item
     count -= 1
 
@@ -51,3 +61,10 @@ for i in m:
     print(i, m[i][0], m[i][5])
 
 print(math.prod(sorted(([m[i][5] for i in m]))[-2:]))
+print("nlcm:",nlcm)
+#print(solve(monkeys, 20, floordiv, 3))  // 3
+#
+#// 3
+#% <nejake cislo>
+#
+#print(solve(monkeys, 10_000, mod, lcm(*(m[TEST] for m in monkeys))))  # Use least common multiple
